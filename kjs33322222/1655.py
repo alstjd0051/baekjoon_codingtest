6696505bca90e306e83c9cae45c3ff210962d7d8
kjs33 <kjs33322222@Gmail.com>
2026-04-14 14:32:15 +0900

    feat: 1655 가운데를 말해요 풀이

--- kjs33322222/1655.py
+++ kjs33322222/1655.py
@@ -1,7 +1,25 @@
 import sys
+import heapq
 input = sys.stdin.readline
 
 n = int(input())
 
+left = []
+right = []
+
 for _ in range(n):
     x = int(input())
+
+    if len(left) == len(right):
+        heapq.heappush(left, -x)
+    else:
+        heapq.heappush(right, x)
+
+    if right and -left[0] > right[0]:
+        left_top = -heapq.heappop(left)
+        right_top = heapq.heappop(right)
+
+        heapq.heappush(left, -right_top)
+        heapq.heappush(right, left_top)
+
+    print(-left[0])
